35dd3505a95e4829ab0f761b9f683a3809913b67
ChoiHyeSung <gptjd0204@naver.com>
2021-06-10 11:07:01 +0900

    Final Commit

--- app/src/main/java/com/example/android_home_iot_smartmirror/ui/AddMemoActivitiy.java
+++ app/src/main/java/com/example/android_home_iot_smartmirror/ui/AddMemoActivitiy.java
@@ -1,5 +1,7 @@
 package com.example.android_home_iot_smartmirror.ui;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -39,6 +41,9 @@ public class AddMemoActivitiy extends AppCompatActivity {
     String urlStr;
     final static String TAG = "AndroidAPITest";
     Toolbar toolbar;
+    String ipAddress = "192.168.0.39";
+    //String ipAddress;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,11 +54,13 @@ public class AddMemoActivitiy extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("메모 추가 및 삭제"); //타이틀 없음
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
+        //TextView mirrorIp = (TextView)findViewById(R.id.mirrorIp);
+        //mirrorIp.setText(ipAddress);
 
 
         Button updateMemoBtn = findViewById(R.id.updateMemoBtn);
         updateMemoBtn.setOnClickListener(new View.OnClickListener() {
-            EditText ip = (EditText)findViewById(R.id.ipAddress);
+            //EditText ip = (EditText)findViewById(R.id.ipAddress);
             EditText memo = (EditText)findViewById(R.id.memo);
 
 
@@ -69,9 +76,10 @@ public class AddMemoActivitiy extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                String test1 = ip.getText().toString();
+                //String test1 = ip.getText().toString();
                 String test2 = memo.getText().toString();
-                urlStr = "http://" + test1 + ":8080/AddMemo?memoTitle=일정&item=" + test2 +"&level=INFO";
+                //urlStr = "http://" + test1 + ":8080/AddMemo?memoTitle=일정&item=" + test2 +"&level=INFO";
+                urlStr = "http://" + ipAddress + ":8080/AddMemo?memoTitle=일정&item=" + test2 +"&level=INFO";
                 new GetMemo(AddMemoActivitiy.this, urlStr).execute();
                 //requestHttpGet(urlStr);
                 //Toast.makeText(getApplicationContext(),urlStr, Toast.LENGTH_SHORT).show();
@@ -81,8 +89,8 @@ public class AddMemoActivitiy extends AppCompatActivity {
 
         Button deleteMemoBtn = findViewById(R.id.deleteMemoBtn);
         deleteMemoBtn.setOnClickListener(new View.OnClickListener() {
-            EditText ip = (EditText)findViewById(R.id.ipAddress);
-            EditText memo = (EditText)findViewById(R.id.memo);
+            //EditText ip = (EditText)findViewById(R.id.ipAddress);
+            //EditText memo = (EditText)findViewById(R.id.memo);
 
 
 
@@ -97,11 +105,11 @@ public class AddMemoActivitiy extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                String test1 = ip.getText().toString();
-                urlStr = "http://" + test1 + ":8080/RemoveMemo?memoTitle=일정&numbers&item=1";
+                //String test1 = ip.getText().toString();
+                urlStr = "http://" + ipAddress + ":8080/RemoveMemo?memoTitle=일정&numbers&item=1";
                 new GetMemo(AddMemoActivitiy.this, urlStr).execute();
                 //requestHttpGet(urlStr);
-                //Toast.makeText(getApplicationContext(),urlStr, Toast.LENGTH_SHORT).show();
+               // Toast.makeText(getApplicationContext(),urlStr, Toast.LENGTH_SHORT).show();
                 Toast.makeText(getApplicationContext(),"메모가 삭제되었습니다", Toast.LENGTH_SHORT).show();
             }
         });
@@ -136,10 +144,55 @@ public class AddMemoActivitiy extends AppCompatActivity {
     }
 */
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.memo_ip_item, menu);
+        return true;
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
+            case R.id.memo_settings1:
+                final EditText txtEdit = new EditText(AddMemoActivitiy.this);
+                txtEdit.setText(ipAddress);
+                txtEdit.setHint("192.168.0.39");
+
+                AlertDialog.Builder clsBuilder = new AlertDialog.Builder(AddMemoActivitiy.this);
+                clsBuilder.setTitle( "미러 IP 설정" );
+                clsBuilder.setView( txtEdit );
+                clsBuilder.setPositiveButton("확인",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick( DialogInterface dialog, int which) {
+                                ipAddress = txtEdit.getText().toString();
+                                // QQQ: 입력된 문자열 처리하기
+                                //TextView mirrorIp = (TextView)findViewById(R.id.mirrorIp);
+                                //mirrorIp.setText(ipAddress);
+                            }
+                        });
+                clsBuilder.setNegativeButton("취소",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        });
+                clsBuilder.show();
+                return true;
+            case R.id.memo_settings2:
+
+                AlertDialog.Builder clsBuilder1 = new AlertDialog.Builder(AddMemoActivitiy.this);
+                clsBuilder1.setTitle( "현재 미러 IP 주소" );
+                clsBuilder1.setMessage(ipAddress);
+
+                clsBuilder1.setPositiveButton("확인",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick( DialogInterface dialog, int which) {
+                            }
+                        });
+                clsBuilder1.show();
+                return true;
             case android.R.id.home: //toolbar의 back키 눌렀을 때 동작
                 finish();
                 return true;
